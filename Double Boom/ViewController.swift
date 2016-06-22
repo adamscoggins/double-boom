68c16ecb660033f7e7437d25a8d32ba67c18acd5
Mac Owner <imac@iMacs-iMac-2.local>
2016-06-22 15:09:39 -0500

    Initial commit

--- Double Boom/ViewController.swift
+++ Double Boom/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var blueBombImg: UIImageView!
+    @IBOutlet weak var redBombImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueBombImg.hidden = true
+    }
+    
+    @IBAction func hideRed(sender: AnyObject) {
+        redBombImg.hidden = true
+    }
 }
 
